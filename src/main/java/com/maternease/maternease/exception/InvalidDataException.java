3882b3ae452a02ecdc2f5fec355270a7da0588bb
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-10-24 21:33:58 +0530

    Error handiling

---
+++ src/main/java/com/maternease/maternease/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.maternease.maternease.exception;
+
+public class InvalidDataException extends RuntimeException{
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
