1811d06208f1d4abbc849749227905dc2038e3c1
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-11-01 23:56:49 +0530

    Some updates

---
+++ src/main/java/com/maternease/maternease/service/ChildService.java
@@ -0,0 +1,7 @@
+//package com.maternease.maternease.service;
+//
+//import com.maternease.maternease.dto.response.ResChildProfileDTO;
+//
+//public interface ChildService {
+////    ResChildProfileDTO getChildProfile(String childId);
+//}
