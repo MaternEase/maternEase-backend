8dec4ea13d0f0ed48f43add1ac9d922b0fd941e0
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-11-06 20:25:05 +0530

    Ouruser dto update

--- src/main/java/com/maternease/maternease/dto/OurUsersDTO.java
+++ src/main/java/com/maternease/maternease/dto/OurUsersDTO.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Data
 public class OurUsersDTO {
 
-    private OurUsersIdDTO ourUsersId;
+//    private OurUsersIdDTO ourUsersId;
     private String fullName;
     private String email;
     private String address;
@@ -22,10 +22,10 @@ public class OurUsersDTO {
     private int age;
     private String motherId;
     private int birth_order;
-    private String clinicId;
+//    private String clinicId;
     private String guardianName;
     private String clinicName;
 
 
-    private ClinicDTO clinic;
+//    private ClinicDTO clinic;
 }
