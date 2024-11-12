cf74b04c9a385411e2092715776dade7aeb79a91
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-11-13 00:26:27 +0530

    do small changes in UserManagementController

--- src/main/java/com/maternease/maternease/controller/UserManagementController.java
+++ src/main/java/com/maternease/maternease/controller/UserManagementController.java
@@ -16,15 +16,15 @@ public class UserManagementController {
     @Autowired
     private UsersManagementService usersManagementService;
 
-    @PostMapping("/auth/midwife-register")
-    public ResponseEntity<ReqRes> midwifeRegister(@RequestBody ReqRes req) {
-        return ResponseEntity.ok(usersManagementService.midwifeRegister(req));
-    }
+//    @PostMapping("/auth/midwife-register")
+//    public ResponseEntity<ReqRes> midwifeRegister(@RequestBody ReqRes req) {
+//        return ResponseEntity.ok(usersManagementService.midwifeRegister(req));
+//    }
 
-    @PostMapping("/auth/doctor-register")
-    public ResponseEntity<ReqRes> doctorRegister(@RequestBody ReqRes req) {
-        return ResponseEntity.ok(usersManagementService.doctorRegister(req));
-    }
+//    @PostMapping("/auth/doctor-register")
+//    public ResponseEntity<ReqRes> doctorRegister(@RequestBody ReqRes req) {
+//        return ResponseEntity.ok(usersManagementService.doctorRegister(req));
+//    }
 
     @PostMapping("/auth/login")
     public ResponseEntity<ReqRes> login(@RequestBody ReqRes req) {
