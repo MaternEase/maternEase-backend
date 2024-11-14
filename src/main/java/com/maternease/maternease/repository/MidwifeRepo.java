2258197e19fcaa48c13eba76a2bd90b116cbbd2c
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-11-14 12:32:17 +0530

    Midiwife id create in the regitration

--- src/main/java/com/maternease/maternease/repository/MidwifeRepo.java
+++ src/main/java/com/maternease/maternease/repository/MidwifeRepo.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MidwifeRepo extends JpaRepository<Midwife,Integer> {
@@ -14,4 +15,6 @@ public interface MidwifeRepo extends JpaRepository<Midwife,Integer> {
 
     @Query("SELECT m FROM Midwife m WHERE SIZE(m.assignedClinics) < :maxClinicAssignments")
     List<Midwife> findAssignableMidwives(@Param("maxClinicAssignments") int maxClinicAssignments);
+
+    Optional<Midwife> findFirstByOrderByMidIdDesc();
 }
