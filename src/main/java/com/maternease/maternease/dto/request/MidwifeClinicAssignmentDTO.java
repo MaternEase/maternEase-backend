34e6de034c1d407b30ce41eabba699747adc4eb1
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-11-14 11:26:18 +0530

    Dto correction

--- src/main/java/com/maternease/maternease/dto/request/MidwifeClinicAssignmentDTO.java
+++ src/main/java/com/maternease/maternease/dto/request/MidwifeClinicAssignmentDTO.java
@@ -1,42 +1,10 @@
 package com.maternease.maternease.dto.request;
 
+import lombok.Data;
+
+@Data
 public class MidwifeClinicAssignmentDTO {
 
     private int midwifeId;
     private int clinicId;
-
-    // Default constructor
-    public MidwifeClinicAssignmentDTO() {
-    }
-
-    // Parameterized constructor
-    public MidwifeClinicAssignmentDTO(int midwifeId, int clinicId) {
-        this.midwifeId = midwifeId;
-        this.clinicId = clinicId;
-    }
-
-    // Getters and Setters
-    public int getMidwifeId() {
-        return midwifeId;
-    }
-
-    public void setMidwifeId(int midwifeId) {
-        this.midwifeId = midwifeId;
-    }
-
-    public int getClinicId() {
-        return clinicId;
-    }
-
-    public void setClinicId(int clinicId) {
-        this.clinicId = clinicId;
-    }
-
-    @Override
-    public String toString() {
-        return "MidwifeClinicAssignmentDTO{" +
-                "midwifeId=" + midwifeId +
-                ", clinicId=" + clinicId +
-                '}';
-    }
 }
