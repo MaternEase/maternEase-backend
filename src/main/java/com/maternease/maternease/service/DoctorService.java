3a6a71f4ceeecb7095b6b182885d9ce8167b20c5
HimashaRandika <himasharandika4@gmail.com>
2024-11-26 20:03:28 +0530

    Add DoctorService

---
+++ src/main/java/com/maternease/maternease/service/DoctorService.java
@@ -0,0 +1,12 @@
+package com.maternease.maternease.service;
+
+import com.maternease.maternease.dto.response.DMotherTableDTO;
+import com.maternease.maternease.dto.response.EMotherTableDTO;
+
+import java.util.List;
+
+public interface DoctorService {
+    List<EMotherTableDTO> getAllExpectedMother();
+
+    List<DMotherTableDTO> getAllDeliveredMother();
+}
