97b799bb549cfc532e3d1deb1a5f82da382ccae0
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-11-26 21:35:23 +0530

    create mailConfic file

---
+++ src/main/java/com/maternease/maternease/config/MailConfig.java
@@ -0,0 +1,28 @@
+package com.maternease.maternease.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("anuththara1000@gmail.com");
+        mailSender.setPassword("Love2002@");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
+    }
+}
