81a786691861100a0848848cc52a23f387fc103c
HimashaRandika <himasharandika4@gmail.com>
2024-12-02 16:47:21 +0530

    Add Recommendation response

---
+++ src/main/java/com/maternease/maternease/dto/response/RecommendationResponse.java
@@ -0,0 +1,14 @@
+package com.maternease.maternease.dto.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RecommendationResponse {
+    // Getters and setters
+    private Long id;
+    private String text;
+
+}
