77f841fe2cce806784eb21804c099470f5cd5fee
HimashaRandika <himasharandika4@gmail.com>
2024-12-02 16:47:45 +0530

    add Recommendation entity

---
+++ src/main/java/com/maternease/maternease/entity/Recommendation.java
@@ -0,0 +1,33 @@
+package com.maternease.maternease.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
