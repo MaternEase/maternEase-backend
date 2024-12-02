7529ac6cb05ff4fdf0670af597f972de315c3540
HimashaRandika <himasharandika4@gmail.com>
2024-12-02 16:46:45 +0530

    Add Recommendation Request

---
+++ src/main/java/com/maternease/maternease/dto/request/RecommendationRequest.java
@@ -0,0 +1,13 @@
+package com.maternease.maternease.dto.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RecommendationRequest {
+    // Getters and setters
+    private String text;
+
+}
